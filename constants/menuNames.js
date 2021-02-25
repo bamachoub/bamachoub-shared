5177fbfa5530d83abf20e30891b1e1abca41512c
hayyan hami <hayyaun@outlook.com>
2021-02-25 23:35:15 +0330

    first commit

---
+++ constants/menuNames.js
@@ -0,0 +1,15 @@
+export const menuNames = {
+  شرکت: 'company',
+  'جنس ورق': 'sheetSelect',
+  ' رنگ': 'color',
+  ابعاد: 'dimensionsSelect',
+  ضخامت: 'thicknessSelect',
+  'نوع رویه': 'coverSelect',
+  ' کیفیت': 'quality',
+  ' یک یا دو رو': 'sideSheetType',
+  ' تعداد در پالت': 'numberInPalet',
+  // "کشورسازنده ورق": "sheetCountry",
+  ' کشورسازنده رویه': 'coverCountry',
+  ' دانسیته': 'density',
+  ' وزن': 'weight',
+};
