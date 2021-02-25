220457e58b572e2324d56205f62c6141f6a3fd47
hayyan hami <hayyaun@outlook.com>
2021-02-25 23:52:30 +0330

    add fonts and constants

---
+++ constants/theming.js
@@ -0,0 +1,12 @@
+export const colors = {
+  primary: '#E1722D',
+  light: '#F5D4C0',
+  dark: '#313638',
+  grey: '#F2EDED',
+  background: '#ffffff',
+  error: '#B00020',
+  info: '#33A1FD',
+  success: '#00982B',
+  inactive: '#414648',
+  primaryDark: '#9c430d',
+};
