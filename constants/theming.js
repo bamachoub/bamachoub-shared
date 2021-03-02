6ba39eb45bb46f08ac7e9025a8e78fc56853f79c
hayyan hami <hayyaun@outlook.com>
2021-03-02 04:36:41 +0330

    add new icon-set

--- constants/theming.js
+++ constants/theming.js
@@ -10,3 +10,30 @@ export const colors = {
   inactive: '#414648',
   primaryDark: '#9c430d',
 };
+
+export const theme = {
+  curves: [18, 14, 8, 4],
+  spaceing: [4, 8, 12, 14, 18, 22, 28, 32, 38],
+  fontSizes: {
+    h1: 44,
+    h2: 32,
+    h3: 24,
+    h4: 20,
+    h5: 18,
+    h6: 16,
+    text: 14,
+    md: 12,
+    sm: 11,
+    xs: 9,
+  },
+  maxWidth: 400,
+};
+
+export const fonts = {
+  IRANSansMobile: 'IRANSansMobile',
+  IRANSansMobile_Black: 'IRANSansMobile_Black',
+  IRANSansMobile_Bold: 'IRANSansMobile_Bold',
+  IRANSansMobile_Light: 'IRANSansMobile_Light',
+  IRANSansMobile_Medium: 'IRANSansMobile_Medium',
+  IRANSansMobile_UltraLight: 'IRANSansMobile_UltraLight',
+};
---
+++ iconsets/Bama/Bama.js
@@ -0,0 +1,4 @@
+import { createIconSetFromIcoMoon } from 'react-native-vector-icons';
+import icoMoonConfig from './selection.json';
+const Icon = createIconSetFromIcoMoon(icoMoonConfig, 'Bama', 'Bama.ttf');
+export default Icon;
